537e50f5c60e7543d16915b03795d038325ea870
Giuliano Schaerlaecken <giuliano.schaerlaecken@student.ehb.be>
2024-09-27 15:16:33 +0200

    oefening af

---
+++ exercises/1-1-signature.js
@@ -0,0 +1,256 @@
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+
+background();
+box1();
+box2();
+box3();
+box4();
+box5();
+box6();
+box7();
+box8();
+box9();
+box10();
+box11();
+box12();
+box13();
+box14();
+box15();
+box16();
+box17();
+box18();
+box19();
+box20();
+box21();
+box22();
+box23();
+box24();
+box25();
+box26();
+box27();
+box28();
+box29();
+box30();
+
+
+function background() {
+
+
+    context.beginPath();
+    context.rect(100, 100, 300, 300);
+    context.fillStyle = "black";
+    context.fill();
+    
+}
+
+
+
+
+
+
+
+function box1() {
+
+
+    context.beginPath();
+    context.rect(125, 125, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+    
+
+}
+
+
+function box2() {
+
+
+    context.beginPath();
+    context.rect(175, 125, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+function box3() {
+
+
+    context.beginPath();
+    context.rect(225, 125, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+    
+
+}
+
+function box4() {
+
+
+    context.beginPath();
+    context.rect(275, 125, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+
+function box5() {
+
+
+    context.beginPath();
+    context.rect(325, 125, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+
+function box6() {
+
+
+    context.beginPath();
+    context.rect(125, 175, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+    
+
+}
+
+
+function box7() {
+
+
+    context.beginPath();
+    context.rect(175, 175, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+function box8() {
+
+
+    context.beginPath();
+    context.rect(225, 175, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+    
+
+}
+
+function box9() {
+
+
+    context.beginPath();
+    context.rect(275, 175, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+
+function box10() {
+
+
+    context.beginPath();
+    context.rect(325, 175, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+    
+
+}
+
+
+
+
+function box11() {
+
+
+    context.beginPath();
+    context.rect(125, 225, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+  
+
+}
+
+
+
+
+function box12() {
+
+
+    context.beginPath();
+    context.rect(325, 225, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+
+function box13() {
+
+
+    context.beginPath();
+    context.rect(175, 275, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+
+function box14() {
+
+
+    context.beginPath();
+    context.rect(275, 275, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+    
+
+}
+
+
+function box15() {
+
+
+    context.beginPath();
+    context.rect(125, 325, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+function box16() {
+
+
+    context.beginPath();
+    context.rect(225, 325, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+   
+
+}
+
+
+function box17() {
+
+
+    context.beginPath();
+    context.rect(325, 325, 50, 50);
+    context.fillStyle = "green";
+    context.fill();
+  
+
+}
